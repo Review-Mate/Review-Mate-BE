6990cc3a2a781b0b75a2f6f22b68210f2f7bbb92
Kwon770 <sckwon770@gmail.com>
2023-08-03 11:36:43 +0900

    init

---
+++ src/main/java/com/somartreview/reviewmate/ReviewmateApplication.java
@@ -0,0 +1,13 @@
+package com.somartreview.reviewmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somartreview/reviewmate/ReviewmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somartreview.reviewmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
