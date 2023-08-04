80809fc2522410d890c060bcd8e51cfeac31615c
Kwon770 <sckwon770@gmail.com>
2023-08-04 23:49:16 +0900

    fix: Correct incorrectly written user_id to customer_id

--- src/main/java/com/somartreview/reviewmate/domain/customer/Customer.java
+++ src/main/java/com/somartreview/reviewmate/domain/customer/Customer.java
@@ -11,7 +11,7 @@ import lombok.Getter;
 public class Customer {
 
     @Id @GeneratedValue
-    @Column(name = "user_id")
+    @Column(name = "customer_id")
     private Long id;
 
     private Long clientSideUserId;
