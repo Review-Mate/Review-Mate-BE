7da4cd724ae93d1081341c483906d3e88f33e25f
Kwon770 <sckwon770@gmail.com>
2023-08-04 23:01:57 +0900

    feat: Add Customer Entity with H2 Database setting

---
+++ src/main/java/com/somartreview/reviewmate/domain/customer/Customer.java
@@ -0,0 +1,28 @@
+package com.somartreview.reviewmate.domain.customer;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Customer {
+
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    private Long clientSideUserId;
+
+    @Column(length = 100)
+    private String username;
+
+    @Column(length = 20)
+    private String phoneNumber;
+
+    @Column(length = 100)
+    private String kakaoId;
+
+}
