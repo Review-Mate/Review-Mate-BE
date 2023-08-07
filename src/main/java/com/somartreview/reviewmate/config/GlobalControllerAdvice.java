fdda763f42a2bbcc8f9d4adf9ad9b49c97d36e89
Kwon770 <sckwon770@gmail.com>
2023-08-07 11:54:38 +0900

    feat: Add web exception handler

--- src/main/java/com/somartreview/reviewmate/config/GlobalControllerAdvice.java
+++ src/main/java/com/somartreview/reviewmate/config/GlobalControllerAdvice.java
@@ -9,10 +9,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.List;
 import java.util.Set;
@@ -26,7 +28,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ExceptionResponse> handleConstraintViolation(ConstraintViolationException e) {
-        log.warn(e.getMessage());
+        log.warn(INVALID_PROPERTY_ERROR.toString() + " : " + e.getMessage());
 
         String messages = extractErrorMessages(e.getConstraintViolations());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -36,9 +38,15 @@ public class GlobalControllerAdvice {
                         .build());
     }
 
+    private String extractErrorMessages(Set<ConstraintViolation<?>> constraintViolations) {
+        return constraintViolations.stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining(", "));
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handleMismatchedInput(MethodArgumentNotValidException e) {
-        log.warn(e.getMessage());
+        log.warn(INVALID_PROPERTY_ERROR.toString() + " : " + e.getMessage());
 
         String messages = extractErrorMessages(e.getBindingResult().getFieldErrors());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -55,11 +63,28 @@ public class GlobalControllerAdvice {
                 .collect(Collectors.joining(","));
     }
 
-    private String extractErrorMessages(Set<ConstraintViolation<?>> constraintViolations) {
-        return constraintViolations.stream()
-                .map(ConstraintViolation::getMessage)
-                .collect(Collectors.joining(", "));
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    public ResponseEntity<ExceptionResponse> handleMismatchedInput(Exception e) {
+        log.warn(INVALID_PROPERTY_ERROR.toString() + " : " + e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ExceptionResponse.builder()
+                        .code(INVALID_PROPERTY_ERROR.getCode())
+                        .message(INVALID_PROPERTY_ERROR.toString() + " : " + "잘못된 요청입니다.")
+                        .build();
     }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ExceptionResponse> noHandlerFoundHandle(NoHandlerFoundException e) {
+        log.warn(API_NOT_FOUND_ERROR.toString() + " : " + e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ExceptionResponse.builder()
+                        .code(API_NOT_FOUND_ERROR.getCode())
+                        .message(API_NOT_FOUND_ERROR.toString() + " : " + "처리할 수 없는 요청입니다.")
+                        .build();
+    }
+
 
     @ExceptionHandler(ReviewMateException.class)
     public ResponseEntity<ExceptionResponse> handleReviewMateException(ReviewMateException e) {
--- src/main/java/com/somartreview/reviewmate/exception/ErrorCode.java
+++ src/main/java/com/somartreview/reviewmate/exception/ErrorCode.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 public enum ErrorCode {
 
     INVALID_PROPERTY_ERROR("9001", "잘못된 값이 입력되었습니다."),
+    API_NOT_FOUND_ERROR("9002", "요청한 API가 존재하지 않습니다"),
 
     REVIEW_MATE_ERROR("9908", "처리하지 못한 예외입니다."),
     RUNTIME_ERROR("9909", "알 수 없는 예외입니다.");
