1e9e7b032d13fda5e9f3db1cbc63c87090ce0c48
Kwon770 <sckwon770@gmail.com>
2023-08-08 00:11:20 +0900

    fix: Rename wrong main application class name

--- src/main/java/com/somartreview/reviewmate/ReviewMateApplication.java
+++ src/main/java/com/somartreview/reviewmate/ReviewMateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReviewmateApplication {
+public class ReviewMateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReviewmateApplication.class, args);
+		SpringApplication.run(ReviewMateApplication.class, args);
 	}
 
 }
