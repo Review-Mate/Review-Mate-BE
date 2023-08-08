4fbf5b2c074df7ffdb4c39d891764c1c9cfa9922
Kwon770 <sckwon770@gmail.com>
2023-08-08 20:52:43 +0900

    feat: Add LiveFeedback validation

--- src/main/java/com/somartreview/reviewmate/domain/LiveFeedback/LiveFeedback.java
+++ src/main/java/com/somartreview/reviewmate/domain/LiveFeedback/LiveFeedback.java
@@ -3,6 +3,8 @@ package com.somartreview.reviewmate.domain.LiveFeedback;
 import com.somartreview.reviewmate.domain.BaseEntity;
 import com.somartreview.reviewmate.domain.Customer.Customer;
 import com.somartreview.reviewmate.domain.TravelProduct.TravelProduct;
+import com.somartreview.reviewmate.exception.DomainLogicException;
+import com.somartreview.reviewmate.exception.ErrorCode;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class LiveFeedback extends BaseEntity {
+
+    private static final int MAX_MESSAGE_LENGTH = 255;
+    private static final int MAX_URL_LENGTH = 1024;
 
     @Id @GeneratedValue
     @Column(name = "live_feedback_id")
@@ -25,13 +30,13 @@ public class LiveFeedback extends BaseEntity {
     private String responseMessage;
 
     @Column(nullable = false)
-    private Boolean isHandled;
+    private Boolean isHandled = false;
 
     @Column(nullable = false)
-    private Boolean isReported;
+    private Boolean isReported = false;
 
     @Column(nullable = false)
-    private Boolean isSolved;
+    private Boolean isSolved = false;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id", nullable = false)
@@ -41,23 +46,60 @@ public class LiveFeedback extends BaseEntity {
     @JoinColumn(name = "travel_product_id", nullable = false)
     private TravelProduct travelProduct;
 
-    public LiveFeedback(String feedbackMessage, String feedbackMediaUrl, String responseMessage, Boolean isHandled, Boolean isReported, Boolean isSolved, Customer customer, TravelProduct travelProduct) {
+    public LiveFeedback(String feedbackMessage, String feedbackMediaUrl, String responseMessage, Customer customer, TravelProduct travelProduct) {
+        validateFeedbackMessage(feedbackMessage);
         this.feedbackMessage = feedbackMessage;
+        validateFeedbackMediaUrl(feedbackMediaUrl);
         this.feedbackMediaUrl = feedbackMediaUrl;
+        validateResponseMessage(responseMessage);
         this.responseMessage = responseMessage;
-        this.isHandled = isHandled;
-        this.isReported = isReported;
-        this.isSolved = isSolved;
         this.customer = customer;
         this.travelProduct = travelProduct;
     }
 
-    public LiveFeedback(String feedbackMessage, String feedbackMediaUrl, String responseMessage, Boolean isHandled, Boolean isReported, Boolean isSolved) {
-        this.feedbackMessage = feedbackMessage;
-        this.feedbackMediaUrl = feedbackMediaUrl;
-        this.responseMessage = responseMessage;
-        this.isHandled = isHandled;
-        this.isReported = isReported;
-        this.isSolved = isSolved;
+    private void validateFeedbackMessage(final String feedbackMessage) {
+        if (feedbackMessage.isBlank() || feedbackMessage.length() > MAX_MESSAGE_LENGTH) {
+            throw new DomainLogicException(ErrorCode.LIVE_FEEDBACK_MESSAGE_ERROR);
+        }
+    }
+
+    private void validateFeedbackMediaUrl(final String feedbackMediaUrl) {
+        if (feedbackMediaUrl.isBlank() || feedbackMediaUrl.length() > MAX_URL_LENGTH) {
+            throw new DomainLogicException(ErrorCode.LIVE_FEEDBACK_MEDIA_URL_ERROR);
+        }
+    }
+
+    private void validateResponseMessage(final String responseMessage) {
+        if (responseMessage == null) {
+            return;
+        }
+
+        if (responseMessage.isBlank() || responseMessage.length() > MAX_MESSAGE_LENGTH) {
+            throw new DomainLogicException(ErrorCode.LIVE_FEEDBACK_MESSAGE_ERROR);
+        }
+    }
+
+    public void handleFeedback() {
+        if (isReported) {
+            throw new DomainLogicException(ErrorCode.LIVE_FEEDBACK_ALREADY_REPORTED_ERROR);
+        }
+
+        isHandled = true;
+    }
+
+    public void reportFeedback() {
+        if (isHandled) {
+            throw new DomainLogicException(ErrorCode.LIVE_FEEDBACK_ALREADY_HANDLED_ERROR);
+        }
+
+        isReported = true;
+    }
+
+    public void solveFeedback() {
+        if (isSolved) {
+            throw new DomainLogicException(ErrorCode.LIVE_FEEDBACK_ALREADY_SOLVED_ERROR);
+        }
+
+        isSolved = true;
     }
 }
--- src/main/java/com/somartreview/reviewmate/exception/ErrorCode.java
+++ src/main/java/com/somartreview/reviewmate/exception/ErrorCode.java
@@ -11,6 +11,12 @@ public enum ErrorCode {
     CUSTOMER_NAME_ERROR("1001", "고객의 이름이 너무 길거나 공백입니다."),
     CUSTOMER_PHONE_NUMBER_ERROR("1002", "고객의 전화번호가 기호를 포함하거나 공백입니다."),
 
+    LIVE_FEEDBACK_MESSAGE_ERROR("1011", "실시간 피드백의 메시지가 너무 길거나 공백입니다."),
+    LIVE_FEEDBACK_MEDIA_URL_ERROR("1012", "실시간 피드백의 미디어 링크가 너무 길거나 공백입니다."),
+    LIVE_FEEDBACK_ALREADY_REPORTED_ERROR("1013", "이미 신고된 실시간 피드백입니다."),
+    LIVE_FEEDBACK_ALREADY_HANDLED_ERROR("1014", "이미 해결된 실시간 피드백입니다."),
+    LIVE_FEEDBACK_ALREADY_SOLVED_ERROR("1015", "이미 해결된 실시간 피드백입니다."),
+
     INVALID_PROPERTY_ERROR("9001", "잘못된 값이 입력되었습니다."),
     API_NOT_FOUND_ERROR("9002", "요청한 API가 존재하지 않습니다"),
 
