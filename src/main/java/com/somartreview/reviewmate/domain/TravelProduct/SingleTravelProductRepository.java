010921c59ef5be72c2919eeb4ccd97b84cd4a02b
Kwon770 <sckwon770@gmail.com>
2023-08-17 00:51:46 +0900

    feat: Add deleteSingleTravelProduct Service

--- src/main/java/com/somartreview/reviewmate/domain/TravelProduct/SingleTravelProductRepository.java
+++ src/main/java/com/somartreview/reviewmate/domain/TravelProduct/SingleTravelProductRepository.java
@@ -3,6 +3,11 @@ package com.somartreview.reviewmate.domain.TravelProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Repository
 public interface SingleTravelProductRepository extends JpaRepository<SingleTravelProduct, Long> {
+
+    Optional<SingleTravelProduct> findByPartnerTravelProductId(String partnerTravelProductId);
 }
--- src/main/java/com/somartreview/reviewmate/service/SingleTravelProductService.java
+++ src/main/java/com/somartreview/reviewmate/service/SingleTravelProductService.java
@@ -2,11 +2,15 @@ package com.somartreview.reviewmate.service;
 
 import com.somartreview.reviewmate.domain.PartnerCompany.PartnerCompany;
 import com.somartreview.reviewmate.domain.PartnerSeller.PartnerSeller;
+import com.somartreview.reviewmate.domain.TravelProduct.SingleTravelProduct;
 import com.somartreview.reviewmate.domain.TravelProduct.SingleTravelProductRepository;
 import com.somartreview.reviewmate.dto.request.travelProduct.SingleTravelProductCreateRequest;
+import com.somartreview.reviewmate.exception.DomainLogicException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.somartreview.reviewmate.exception.ErrorCode.TRAVEL_PRODUCT_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +26,21 @@ public class SingleTravelProductService {
         final PartnerSeller partnerSeller = partnerSellerService.findPartnerSellerById(request.getPartnerSellerId());
 
         return singleTravelProductRepository.save(request.toEntity(partnerCompany, partnerSeller)).getId();
+    }
+
+    @Transactional
+    public void deleteSingleTravelProductById(Long id) {
+        SingleTravelProduct foundTravelProduct = singleTravelProductRepository.findById(id)
+                .orElseThrow(() -> new DomainLogicException(TRAVEL_PRODUCT_NOT_FOUND));
+
+        singleTravelProductRepository.delete(foundTravelProduct);
+    }
+
+    @Transactional
+    public void deleteSingleTravelProductByPartnerProductId(String partnerProductId) {
+        SingleTravelProduct foundTravelProduct = singleTravelProductRepository.findByPartnerTravelProductId(partnerProductId)
+                .orElseThrow(() -> new DomainLogicException(TRAVEL_PRODUCT_NOT_FOUND));
+
+        singleTravelProductRepository.delete(foundTravelProduct);
     }
 }
