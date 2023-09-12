e2352d07fd56127e017905ae582618b313b1c1af
Kwon770 <sckwon770@gmail.com>
2023-09-12 19:15:14 +0900

    feat: Add PackageTravelProductTest

---
+++ src/test/java/com/somartreview/reviewmate/domain/product/PackageTravelProductTest.java
@@ -0,0 +1,36 @@
+package com.somartreview.reviewmate.domain.product;
+
+import com.somartreview.reviewmate.domain.partner.company.PartnerCompany;
+import com.somartreview.reviewmate.domain.partner.seller.PartnerSeller;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PackageTravelProductTest {
+
+    private PackageTravelProduct packageTravelProduct;
+
+    @BeforeEach
+    void setup() {
+        packageTravelProduct = PackageTravelProduct.builder()
+                .partnerCustomId("PRODUCT-0001")
+                .thumbnailUrl("www.thumbnail.com")
+                .name("신라더스테이")
+                .partnerCompany(new PartnerCompany())
+                .partnerSeller(new PartnerSeller())
+                .build();
+    }
+
+    @Test
+    void 패키지_여행상품에_여행코스를_추가한다() {
+        // given
+        TourCourse tourCourse = new TourCourse();
+
+        // when
+        packageTravelProduct.addTourCourse(tourCourse);
+
+        // then
+        assertThat(packageTravelProduct.getTourCourses().size()).isEqualTo(1);
+    }
+}
