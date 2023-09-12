c9faec3caf3428819a8f34e2623b904e77ff4219
Kwon770 <sckwon770@gmail.com>
2023-09-12 23:38:12 +0900

    fix: Remove schema.sql and data.sql from test and Add application-test.yml for DataJpaTest

--- src/test/java/com/somartreview/reviewmate/domain/customer/CustomerRepositoryTest.java
+++ src/test/java/com/somartreview/reviewmate/domain/customer/CustomerRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
@@ -13,8 +15,10 @@ import java.util.Optional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
+@ActiveProfiles("test")
 @DataJpaTest
-@Sql(scripts = {"classpath:schema.sql", "classpath:data.sql"})
+@TestPropertySource(locations = {"classpath:application-test.yml"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CustomerRepositoryTest {
 
     @Autowired
